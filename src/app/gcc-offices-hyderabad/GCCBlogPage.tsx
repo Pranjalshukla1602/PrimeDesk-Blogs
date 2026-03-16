362d52c48d7c6f38e64bdfc41362a9077a63bc34
Priyamkatiyarlu <priyamktr7@gmail.com>
2026-03-16 14:17:38 +0530

    added gcc blog

--- src/app/gcc-offices-hyderabad/GCCBlogPage.tsx
+++ src/app/gcc-offices-hyderabad/GCCBlogPage.tsx
@@ -962,7 +962,7 @@ export default function GCCBlogPage() {
                   <svg viewBox="0 0 24 24"><path d="M22 16.92v3a2 2 0 0 1-2.18 2A19.79 19.79 0 0 1 11.69 19a19.5 19.5 0 0 1-6-6A19.79 19.79 0 0 1 2.12 4.18 2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72c.127.96.361 1.903.7 2.81a2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45c.907.339 1.85.573 2.81.7A2 2 0 0 1 22 16.92z" /></svg>
                   Call Now
                 </a>
-                <a href="https://wa.me/918978426302" target="_blank" rel="noopener noreferrer" className="nm-action-btn nm-btn-green">
+                <a href="https://wa.me/918978426302?text=Hello,%20I%20would%20like%20to%20inquire%20about%20GCC%20office%20spaces%20in%20Hyderabad." target="_blank" rel="noopener noreferrer" className="nm-action-btn nm-btn-green">
                   <svg viewBox="0 0 24 24"><path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z" /></svg>
                   WhatsApp Chat
                 </a>
